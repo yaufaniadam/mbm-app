3a7caa6874fb8dc4380f6fdf9c772c7139f5f27e
bokerface <waskitodamar17@gmail.com>
2025-10-30 22:43:40 +0700

    ganti status pangan nek wes verif

--- resources/views/filament/production/pages/verify.blade.php
+++ resources/views/filament/production/pages/verify.blade.php
@@ -20,8 +20,19 @@
             Simpan
         </x-filament::button>
     @else
-        <x-filament::badge color="gray" class="mt-4">
-            Produk pangan tidak memenuhi kriteria
-        </x-filament::badge>
+        @if ($record->status == 'Ditolak')
+            <x-filament::badge color="gray" class="mt-4">
+                Produk pangan tidak memenuhi kriteria
+            </x-filament::badge>
+        @elseif($record->status == 'Terverifikasi')
+            <x-filament::badge color="success" class="mt-4">
+                Makanan lolos kriteria dan siap untuk didistribusikan
+            </x-filament::badge>
+        @else
+            <x-filament::badge color="primary" class="mt-4">
+                Makanan sudah didistribusikan
+            </x-filament::badge>
+        @endif
+
     @endif
 </x-filament-panels::page>
