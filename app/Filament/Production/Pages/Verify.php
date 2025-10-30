d50bb561d4113c9aa3b3e40afe83f40cdabcfb94
bokerface <waskitodamar17@gmail.com>
2025-10-30 18:47:28 +0700

    gae infolist nggo menu hari ini

--- app/Filament/Production/Pages/Verify.php
+++ app/Filament/Production/Pages/Verify.php
@@ -2,15 +2,51 @@
 
 namespace App\Filament\Production\Pages;
 
+use App\Models\ProductionSchedule;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Infolists\Components\TextEntry;
 use Filament\Pages\Page;
+use Filament\Schemas\Schema;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\HtmlString;
 
-class Verify extends Page
+class Verify extends Page implements HasForms
 {
+    use InteractsWithForms;
+
     protected string $view = 'filament.production.pages.verify';
+
+    protected ?string $heading = '';
+
+    public ?array $data = [];
+
+    public $menuData = null;
 
     public static function getNavigationIcon(): ?string
     {
         return 'heroicon-o-check-badge';
+    }
+
+    public function getFormSchema()
+    {
+        return [
+            // TextEntry::make('menu_hari_ini')->color('primary'),
+            Select::make('status')
+                ->options([
+                    "Ditolak" => "Ditolak",
+                    "Terverifikasi" => "Terverifikasi",
+                ])
+        ];
+    }
+
+    public function mount()
+    {
+        $user = Auth::user();
+
+        $organizationId = $user->unitTugas()->first()->id;
+
+        $this->menuData = ProductionSchedule::where('sppg_id', $organizationId)->first();
     }
 }
--- resources/views/filament/production/pages/verify.blade.php
+++ resources/views/filament/production/pages/verify.blade.php
@@ -1,3 +1,13 @@
 <x-filament-panels::page>
     {{-- Page content --}}
+
+    <div class="">
+        <p>
+            Menu hari {{ Carbon\Carbon::parse($menuData->tanggal)->locale('id')->translatedFormat('l, d F Y') }}
+        </p>
+    </div>
+
+    <form>
+        {{ $this->form }}
+    </form>
 </x-filament-panels::page>
