0c4423181b63afd8d24e77ff93a5406fb91909fe
bokerface <waskitodamar17@gmail.com>
2025-11-06 17:55:19 +0700

    ngilangi collapsible sidebar neng sppg panel ben ra kakean cookies

--- app/Providers/Filament/SppgPanelProvider.php
+++ app/Providers/Filament/SppgPanelProvider.php
@@ -60,7 +60,6 @@ class SppgPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ])
-            ->sidebarCollapsibleOnDesktop()
             ->spa(hasPrefetching: true);
     }
 }
