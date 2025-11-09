d4d93519f2db96bb73d346336e658548f75b8e69
bokerface <waskitodamar17@gmail.com>
2025-11-10 00:11:30 +0700

    ngenehi default password pass admin gae user anyar

--- app/Filament/Admin/Resources/Users/Pages/CreateUsers.php
+++ app/Filament/Admin/Resources/Users/Pages/CreateUsers.php
@@ -8,4 +8,10 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateUsers extends CreateRecord
 {
     protected static string $resource = UsersResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['password'] = 'p4$$w0rd'; // Set default password
+        return $data;
+    }
 }
