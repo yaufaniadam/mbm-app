65bef35e7357c708d69e1fe2c4b3567eef9fa445
bokerface <waskitodamar17@gmail.com>
2025-12-18 22:03:10 +0700

    nambahi observer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\OperatingExpense;
+use App\Models\Remittance;
+use App\Models\SppgIncomingFund;
+use App\Observers\FinancialObserver;
 use Filament\Support\Assets\Css;
 use Filament\Support\Assets\Js;
 use Filament\Support\Facades\FilamentAsset;
@@ -26,5 +30,9 @@ class AppServiceProvider extends ServiceProvider
             Css::make('leaflet-css', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css'),
             Js::make('leaflet-js', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'),
         ]);
+
+        SppgIncomingFund::observe(FinancialObserver::class);
+        OperatingExpense::observe(FinancialObserver::class);
+        Remittance::observe(FinancialObserver::class);
     }
 }
